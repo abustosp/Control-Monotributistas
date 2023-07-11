8178b505a576bd0293c0830ae2322f066be6d249
Agustin Bustos Piasentini <63634574+abustosp@users.noreply.github.com>
2023-07-10 22:31:02 -0300

    Update Script

--- LIB/Extractor_Facturas.py
+++ LIB/Extractor_Facturas.py
@@ -23,48 +23,53 @@ with open("lista_archivos.txt", "w") as output:
     output.write(str(lista_archivos))
 
 # Crear un dataframe vacío
-df = pd.DataFrame(columns=["Archivo", "COD" , "Punto de Venta", "Número de Factura", "Fecha", "Desde" , "Hasta"])
+df = pd.DataFrame(columns=["Archivo", "CUIT del Emisor" , "COD" , "Punto de Venta", "Número de Factura", "Fecha", "Desde" , "Hasta"])
 
 # Extraer el texto de los archivos PDF solamente de la primera página
 for i in lista_archivos:
     with pdfplumber.open(i) as pdf:
         primera_pagina = pdf.pages[0]
         texto = primera_pagina.extract_text()
-        print(texto)
-        print("--------------------------------------------------")
+        #print(texto)
+        #print("--------------------------------------------------")
         
         Archivo = i.split("/")[-1].replace(".pdf", "")
 
         # Extraer el COD de de factura
         Cod = re.search(r"COD. (\d+)", texto)
         Cod = Cod.group(1)
-        print(Cod)
+        #print(Cod)
+
+        # Extraer el CUIT del emisor
+        CUIT = re.search(r"CUIT: (\d+)", texto)
+        CUIT = CUIT.group(1)
+        #print(CUIT)
 
         # Extraer el punto de venta
         punto_venta = re.search(r"Punto de Venta: (\d+)", texto)
         punto_venta = punto_venta.group(1)
-        print(punto_venta)
+        #print(punto_venta)
 
         # Extraer el número de factura
         numero_factura = re.search(r"Comp. Nro: (\d+)", texto)
         numero_factura = numero_factura.group(1)
-        print(numero_factura)
+        #print(numero_factura)
 
         # Extraer la fecha
         fecha = re.search(r"Fecha de Emisión: (\d+/\d+/\d+)", texto)
         fecha = fecha.group(1)
-        print(fecha)
+        #print(fecha)
 
         # Extraer el rango de facturas
         Desde = re.search(r"Desde: (\d+/\d+/\d+)", texto)
         Desde = Desde.group(1)
-        print(Desde)
+        #print(Desde)
         Hasta = re.search(r"Hasta:(\d+/\d+/\d+)", texto)
         Hasta = Hasta.group(1)
-        print(Hasta)
+        #print(Hasta)
 
         # Agregar una linea nueva con los datos extraidos
-        df = pd.concat([df, pd.DataFrame([[Archivo, Cod, punto_venta, numero_factura, fecha, Desde, Hasta]], columns=["Archivo", "COD" , "Punto de Venta", "Número de Factura", "Fecha", "Desde" , "Hasta"])], ignore_index=True)
+        df = pd.concat([df, pd.DataFrame([[Archivo, Cod, CUIT, punto_venta, numero_factura, fecha, Desde, Hasta]], columns=["Archivo", "COD" , "CUIT del emisor" , "Punto de Venta", "Número de Factura", "Fecha", "Desde" , "Hasta"])], ignore_index=True)
 
 
 # Exportar el dataframe a un archivo csv
